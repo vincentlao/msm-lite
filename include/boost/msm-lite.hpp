615200ebcd73f98f9ceb0340b4babc4e725c8639
Krzysztof Jusiak <krzysztof@jusiak.net>
2016-10-11 16:40:51 +0100

    defer

--- include/boost/msm-lite.hpp
+++ include/boost/msm-lite.hpp
@@ -1421,10 +1421,10 @@ struct transition<state<S1>, state<S2>, event<E>, G, A> {
   template <class SM>
   status execute(SM &self, const E &event, aux::byte &current_state) {
     if (call(g, event, self.deps_, self.me_)) {
-      call(a, event, self.deps_, self.me_);
       self.me_.template update_current_state<typename state<S1>::explicit_states>(
           self, current_state, aux::get_id<typename SM::type::states_ids_t, -1, dst_state>(), state<src_state>{},
           state<dst_state>{});
+      call(a, event, self.deps_, self.me_);
       return ret_status<A>();
     }
     return status::NOT_HANDLED;
@@ -1445,10 +1445,10 @@ struct transition<state<S1>, state<S2>, event<E>, always, A> {
   transition(const always &, const A &a) : a(a) {}
   template <class SM>
   status execute(SM &self, const E &event, aux::byte &current_state) {
-    call(a, event, self.deps_, self.me_);
     self.me_.template update_current_state<typename state<S1>::explicit_states>(
         self, current_state, aux::get_id<typename SM::type::states_ids_t, -1, dst_state>(), state<src_state>{},
         state<dst_state>{});
+    call(a, event, self.deps_, self.me_);
     return ret_status<A>();
   }
   A a;
